a9e08d966d5972fabb140fe2e883566adce4221c
victoriaashling <victoria.ashling@gmail.com>
2018-08-09 19:45:05 -0400

    first push

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parse");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
@@ -7,6 +7,29 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+let waitingList = [];
+
+let tables = [
+ {
+   name: "frank",
+   phoneNum: "123-4567",
+   email: "frank@gmail.com",
+   id: 900
+ },
+ {
+   name: "Zarr",
+   phoneNum: "234-4567",
+   email: "zarr@gmail.com",
+   id: 1900
+ },
+ {
+   name: "may",
+   phoneNum: "345-4567",
+   email: "may@gmail.com",
+   id: 800
+ }
+];
 
 
 app.get("/", (request, response) => {
@@ -22,7 +45,15 @@ app.get("/add", (request, response) => {
 });
 
 app.get("/api/tables", (request, response) => {
-    
+    return response.json(tables);
+});
+
+app.post("/api/tables", (request, response) => {
+    let newTable = request.body;
+    tables.push(newTable);
+
+    // response.json(newTable);
+    response.sendFile(path.join(__dirname, "add.html"));
 });
 
 
